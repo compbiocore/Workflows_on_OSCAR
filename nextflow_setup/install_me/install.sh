40299718f1f4e4dccd37bc4385613f885313d090
Jordan <76955330+j-stat@users.noreply.github.com>
2022-09-13 17:56:29 -0400

    Update install.sh

--- nextflow_setup/install_me/install.sh
+++ nextflow_setup/install_me/install.sh
@@ -1,4 +1,6 @@
- # Load needed modules 
+# Back up .bashrc, just in case 
+cp ~/.bashrc ~/.bashrc.bak
+# Load needed modules 
 module load python/3.9.0
 module load java/jdk-11.0.11
 # Run Python script 
