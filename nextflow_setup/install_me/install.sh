55acea99c90a491e0ffad9a759f2826117c063ce
Jordan <76955330+j-stat@users.noreply.github.com>
2022-09-13 18:23:58 -0400

    Delete nextflow_setup/install_me directory

--- nextflow_setup/install_me/install.sh
+++
@@ -1,7 +0,0 @@
-# Back up .bashrc, just in case 
-cp ~/.bashrc ~/.bashrc.bak
-# Load needed modules 
-module load python/3.9.0
-module load java/jdk-11.0.11
-# Run Python script 
-python3 $HOME/nextflow_setup/workflows.py
