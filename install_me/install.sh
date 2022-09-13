11934529a6b0ee4a45651b679cf31a3f3303d3b1
Jordan <76955330+j-stat@users.noreply.github.com>
2022-09-13 18:56:08 -0400

    Update install.sh

--- install_me/install.sh
+++ install_me/install.sh
@@ -4,6 +4,6 @@ cp ~/.bashrc ~/.bashrc.bak
 module load python/3.9.0
 module load java/jdk-11.0.11
 # Move setup folders to HOME directory 
-mv ~/Workflows_on_OSCAR/install_me/nextflow_setup ~/nextflow_setup
+mv ~/workflows_on_OSCAR/install_me/nextflow_setup ~/nextflow_setup
 # Run Python script 
-python3 $HOME/Workflows_on_OSCAR/workflows.py
+python3 $HOME/workflows_on_OSCAR/workflows.py
