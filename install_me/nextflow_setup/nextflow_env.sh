222aac6457c5382318e9d94313eb39a61f2defb8
Jordan <76955330+j-stat@users.noreply.github.com>
2022-09-13 18:48:57 -0400

    Update nextflow_env.sh

--- install_me/nextflow_setup/nextflow_env.sh
+++ install_me/nextflow_setup/nextflow_env.sh
@@ -20,4 +20,4 @@ if [ ! -f "$HOME/.workflow_specific_aliases" ]; then touch ~/.workflow_specific_
 echo 'alias nextflow_start="source ~/nextflow_env_${USER}/bin/activate"' >> $HOME/.workflow_specific_aliases 
 echo 'alias nextflow_remove="rm -rf ~/nextflow_env_${USER} ~/.workflow_specific_aliases"' >> $HOME/.workflow_specific_aliases 
 grep -qxF 'if [ -e $HOME/.workflow_specific_aliases ]; then source $HOME/.workflow_specific_aliases; fi' ~/.bashrc || echo 'if [ -e $HOME/.workflow_specific_aliases ]; then source $HOME/.workflow_specific_aliases; fi' >> ~/.bashrc
-exec bash
+exec ~/.bashrc
