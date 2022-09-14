61aaf72794b6e38e3e8f8e71819e0fbec419791c
Jordan <76955330+j-stat@users.noreply.github.com>
2022-09-13 21:53:13 -0400

    Update install.sh

--- install_me/install.sh
+++ install_me/install.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-# Back up .bashrc, just in case 
-cp ~/.bashrc ~/.bashrc.bak
+# First time we run install, back up .bashrc, just in case 
+if [ ! -f "$HOME/.bashrc.bak" ]; then cp ~/.bashrc ~/.bashrc.bak; fi 
 # Copy installation folders 
 cp -r ~/workflows_on_OSCAR/install_me/nextflow_setup ~/nextflow_setup
 # Run Python script 
