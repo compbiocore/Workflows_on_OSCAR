366192bd306e5aa6e0e928aa659b0f285deb834a
Jordan <76955330+j-stat@users.noreply.github.com>
2022-09-14 13:34:09 -0400

    Update install.sh

--- install_me/install.sh
+++ install_me/install.sh
@@ -2,6 +2,8 @@
 
 # First time we run install, back up .bashrc, just in case 
 if [ ! -f "$HOME/.bashrc.bak" ]; then cp ~/.bashrc ~/.bashrc.bak; fi 
+# Create a user-friendly alias to shut off virtual environments 
+echo 'alias quit="deactivate"' >> $HOME/.bashrc
 # Load needed modules 
 module load python/3.9.0
 module load java/jdk-11.0.11
