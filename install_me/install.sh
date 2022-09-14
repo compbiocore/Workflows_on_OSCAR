0558e481f65195c0162e5478e6e7b14f399d86a9
Jordan <76955330+j-stat@users.noreply.github.com>
2022-09-13 21:51:09 -0400

    Update install.sh

--- install_me/install.sh
+++ install_me/install.sh
@@ -2,9 +2,6 @@
 
 # Back up .bashrc, just in case 
 cp ~/.bashrc ~/.bashrc.bak
-# Load needed modules 
-module load python/3.9.0
-module load java/jdk-11.0.11
 # Copy installation folders 
 cp -r ~/workflows_on_OSCAR/install_me/nextflow_setup ~/nextflow_setup
 # Run Python script 
