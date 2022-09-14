b498ab48dd411a5f31b2ef4151a2dd439cbeca03
Jordan <76955330+j-stat@users.noreply.github.com>
2022-09-14 12:20:30 -0400

    Update install.sh

--- install_me/install.sh
+++ install_me/install.sh
@@ -5,8 +5,5 @@ if [ ! -f "$HOME/.bashrc.bak" ]; then cp ~/.bashrc ~/.bashrc.bak; fi
 # Load needed modules 
 module load python/3.9.0
 module load java/jdk-11.0.11
-# Copy installation folders 
-cp -r ~/workflows_on_OSCAR/install_me/nextflow_setup ~/nextflow_setup
-cp -r ~/workflows_on_OSCAR/install_me/snakemake_setup ~/snakemake_setup
 # Run Python script 
 python3 $HOME/workflows_on_OSCAR/workflows.py 
