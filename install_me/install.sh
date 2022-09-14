241ada17a34797fe57df8ad9a9bb8fc9723cef34
Jordan <76955330+j-stat@users.noreply.github.com>
2022-09-14 13:55:36 -0400

    Update install.sh

--- install_me/install.sh
+++ install_me/install.sh
@@ -3,7 +3,8 @@
 # First time we run install, back up .bashrc, just in case 
 if [ ! -f "$HOME/.bashrc.bak" ]; then cp ~/.bashrc ~/.bashrc.bak; fi 
 # Create a user-friendly alias to shut off virtual environments 
-if [ ! "$(type -t quit)" = "alias" ]; then grep -qxF 'alias quit="deactivate"' ~/.bashrc || echo 'alias quit="deactivate"' >> ~/.bashrc; fi 
+if [ ! "$(type -t quit)" = "alias" ]; then echo "$(echo $(type quit)) and is being overwritten"; fi 
+grep -qxF 'alias quit="deactivate"' ~/.bashrc || echo 'alias quit="deactivate"' >> ~/.bashrc; 
 # Load needed modules 
 module load python/3.9.0
 module load java/jdk-11.0.11
