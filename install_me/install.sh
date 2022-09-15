f7cf7c87006cb40fadb0d0ff6bbc929d9dc11bcd
Jordan <76955330+j-stat@users.noreply.github.com>
2022-09-15 14:49:40 -0400

    Update install.sh

--- install_me/install.sh
+++ install_me/install.sh
@@ -6,5 +6,6 @@ if [ ! -f "$HOME/.bashrc.bak" ]; then cp ~/.bashrc ~/.bashrc.bak; fi
 grep -qxF 'alias quit_workflow="deactivate"' ~/.bashrc || echo 'alias quit_workflow="deactivate"' >> ~/.bashrc; 
 # Load needed modules 
 module load python/3.9.0
+module load java/jdk-11.0.11
 # Run Python script 
 python3 $HOME/workflows_on_OSCAR/workflows.py 
