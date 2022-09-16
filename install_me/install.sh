cdd591df2f7f56a26a084a027b4117d456c86156
Jordan <76955330+j-stat@users.noreply.github.com>
2022-09-15 20:05:24 -0400

    Update install.sh

--- install_me/install.sh
+++ install_me/install.sh
@@ -5,6 +5,8 @@ if [ ! -f "$HOME/.bashrc.bak" ]; then cp ~/.bashrc ~/.bashrc.bak; fi
 # Create a user-friendly alias to shut off virtual environments 
 grep -qxF 'alias quit_workflow="deactivate"' ~/.bashrc || echo 'alias quit_workflow="deactivate"' >> ~/.bashrc; 
 # Load needed modules 
+module unload python/2.7.12
+module unload java/8u111
 module load python/3.9.0
 module load java/jdk-11.0.11
 # Run Python script 
