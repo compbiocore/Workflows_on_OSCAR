71819e1d8fd4f1bc72c772e9953cbdfc7e732868
Jordan <76955330+j-stat@users.noreply.github.com>
2022-09-15 20:55:49 -0400

    Update workflows.py

--- workflows.py
+++ workflows.py
@@ -70,7 +70,7 @@ while True:
             # Set up environment, if it doesnt exist 
             try: 
                 os.system('cp -r ~/workflows_on_OSCAR/install_me/nextflow_setup ~/nextflow_setup') 
-                bash_file='bash ' + homedir + '/nextflow_setup/nextflow_env.sh'
+                bash_file=['bash', homedir + '/nextflow_setup/nextflow_env.sh']
                 subprocess.call(bash_file, shell=False)
             except OSError:
                 print("Nextflow software setup error, please contact: jordan_lawson@brown.edu")
