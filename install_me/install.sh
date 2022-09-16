d23f7224bab2131de36ac52e696cf3b314b67d8c
Jordan <76955330+j-stat@users.noreply.github.com>
2022-09-15 22:29:00 -0400

    Update install.sh

--- install_me/install.sh
+++ install_me/install.sh
@@ -3,7 +3,7 @@
 # First time we run install, back up .bashrc, just in case 
 if [ ! -f "$HOME/.bashrc.bak" ]; then cp ~/.bashrc ~/.bashrc.bak; fi 
 # Create a user-friendly alias to shut off virtual environments 
-grep -qxF 'alias quit_workflow="deactivate"' ~/.bashrc || echo 'alias quit_workflow="deactivate"' >> ~/.bashrc; 
+grep -qxF 'alias quit_workflow="deactivate && bash"' ~/.bashrc || echo 'alias quit_workflow="deactivate && bash"' >> ~/.bashrc; 
 # Load needed modules 
 module load python/3.9.0 java/jdk-11.0.11
 # Run Python script 
