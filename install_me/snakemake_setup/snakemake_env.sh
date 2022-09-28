da2cf64748394c35bbb25faa16414954b654868f
Jordan <76955330+j-stat@users.noreply.github.com>
2022-09-28 13:56:05 -0400

    Update snakemake_env.sh

--- install_me/snakemake_setup/snakemake_env.sh
+++ install_me/snakemake_setup/snakemake_env.sh
@@ -25,6 +25,8 @@ fi
 
 # change .bashrc to make it more user-friendly 
 if [ ! -f "$HOME/.snakemake_specific_aliases" ]; then touch ~/.snakemake_specific_aliases; fi 
-echo 'alias snakemake_start="module load python/3.9.0 && source ~/snakemake_env_${USER}/bin/activate"' >> $HOME/.snakemake_specific_aliases 
-echo 'alias snakemake_remove="rm -rf ~/snakemake_env_${USER} ~/snakemake_setup ~/.snakemake_specific_aliases ~/.config/snakemake ~/snakemake_folders; unalias snakemake_start; unalias snakemake_remove"' >> $HOME/.snakemake_specific_aliases 
+echo 'snakemake_start () { module load python/3.9.0 && source ~/snakemake_env_${USER}/bin/activate; }' >> $HOME/.snakemake_specific_aliases 
+echo 'snakemake_remove () { rm -rf ~/snakemake_env_${USER} ~/snakemake_setup ~/.snakemake_specific_aliases ~/.config/snakemake ~/snakemake_folders; unset -f snakemake_start; unset -f snakemake_remove; }' >> $HOME/.snakemake_specific_aliases 
+echo 'export -f snakemake_start' >> $HOME/.snakemake_specific_aliases 
+echo 'export -f snakemake_remove' >> $HOME/.snakemake_specific_aliases
 grep -qxF 'if [ -e $HOME/.snakemake_specific_aliases ]; then source $HOME/.snakemake_specific_aliases; fi' ~/.bashrc || echo 'if [ -e $HOME/.snakemake_specific_aliases ]; then source $HOME/.snakemake_specific_aliases; fi' >> ~/.bashrc
