ce6111724d9fc8abebf7442510fcda1fc3b86c5a
Eric Salomaki <eric.salomaki@gmail.com>
2024-05-03 09:58:53 -0400

    Update python3 module in install.sh

--- install_me/install.sh
+++ install_me/install.sh
@@ -5,6 +5,6 @@ if [ ! -f "$HOME/.bashrc.bak" ]; then cp ~/.bashrc ~/.bashrc.bak; fi
 # Create a user-friendly alias to shut off virtual environments 
 grep -qxF 'alias quit_workflow="deactivate && bash"' ~/.bashrc || echo 'alias quit_workflow="deactivate && bash"' >> ~/.bashrc; 
 # Load needed modules 
-module load python/3.9.0 java/jdk-11
+module load python/3.9.16s-x3wdtvt java/jdk-11
 # Run Python script 
 python3 $HOME/workflows_on_OSCAR/workflows.py 
